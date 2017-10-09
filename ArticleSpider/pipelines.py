f846e9343e72b1a0363d3046fce3257247158f79
_GL0oMY <uestcyyh@sina.com>
2017-10-09 11:58:43 +0800

    新增: pipeline中导出json文件

--- ArticleSpider/pipelines.py
+++ ArticleSpider/pipelines.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+import codecs
+import json
 
 from scrapy.pipelines.images import ImagesPipeline
+from scrapy.exporters import JsonItemExporter
 
 # Define your item pipelines here
 #
@@ -18,4 +21,33 @@ class ArticleImagePipeline(ImagesPipeline):
     def item_completed(self, results, item, info):
         for __, value in results:
             item['front_image_path'] = value['path']
+        return item
+
+
+class JsonWithEncodingPipeline(object):
+    def __init__(self):
+        self.file = codecs.open('article.json', 'w', encoding='utf-8')
+
+    def process_item(self, item, spider):
+        lines = json.dumps(dict(item), ensure_ascii=False) + '\n'
+        self.file.write(lines)
+        return item
+
+    def spider_closed(self, spider):
+        self.file.close()
+
+
+class JsonExporterPipeline(object):
+    """调用scrapy提供的json_exporter导出json文件"""
+    def __init__(self):
+        self.file = open('article_exporter.json', 'wb')
+        self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
+        self.exporter.start_exporting()
+
+    def spider_closed(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
--- ArticleSpider/settings.py
+++ ArticleSpider/settings.py
@@ -67,9 +67,10 @@ ROBOTSTXT_OBEY = False
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'ArticleSpider.pipelines.ArticlespiderPipeline': 300,
    'ArticleSpider.pipelines.ArticleImagePipeline': 1,
    # 'scrapy.pipelines.images.ImagesPipeline': 1,
+   'ArticleSpider.pipelines.JsonExporterPipeline': 2,
+   'ArticleSpider.pipelines.ArticlespiderPipeline': 300,
 }
 
 IMAGES_URLS_FIELD = 'front_image_url'
